3ec6f4f9405d18947c600efead6e59ca934112a4
Danilo Silva <danilo.malheiro@7comm.com.br>
2024-04-20 13:20:27 -0300

    Manutencao de estados

--- MinhaAppFuncional/src/AppMvcFuncional/Controllers/AlunosController.cs
+++ MinhaAppFuncional/src/AppMvcFuncional/Controllers/AlunosController.cs
@@ -17,6 +17,8 @@ namespace AppMvcFuncional.Controllers
 
         public async Task<IActionResult> Index()
         {
+            ViewBag.Sucesso = "Listagem bem sucedida!";
+
             return _context.Aluno != null ?
                         View(await _context.Aluno.ToListAsync()):
                         Problem("Entity set 'ApplicationDbContext.Aluno' is null");
@@ -106,6 +108,9 @@ namespace AppMvcFuncional.Controllers
                         throw;
                     }
                 }
+
+                TempData["Sucesso"] = "Aluno editado com sucesso!";
+
                 return RedirectToAction(nameof(Index));
             }
             return View(aluno);
